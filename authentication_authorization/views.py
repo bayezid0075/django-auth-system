19fb2056efa7f26302e100e63e32212793e607ba
bayezid0075 <geekbayezid@gmail.com>
2025-08-04 09:58:26 +0600

    user signup with django function

--- authentication_authorization/views.py
+++ authentication_authorization/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request): 
     return HttpResponse("This is auth system !!")
+def user_signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("account create done")
+    else:
+        form = UserCreationForm()
+        print("user came to form")
+    return render(request, 'signup.html', {'form': form})
