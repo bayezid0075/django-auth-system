9d3f74da0d296f2cba36937478123fbafd249103
bayezid0075 <geekbayezid@gmail.com>
2025-08-05 09:19:35 +0600

    login with django built in function

--- authentication_authorization/urls.py
+++ authentication_authorization/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',  views.home),
-    path('signup/',  views.user_signup, name='signup')
+    path('signup/',  views.user_signup, name='signup'),
+    path('login/',  views.login, name='login')
 ]
--- authentication_authorization/views.py
+++ authentication_authorization/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as django_login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 def home(request): 
     return HttpResponse("This is auth system !!")
 def user_signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        print(form)
         if form.is_valid():
             form.save()
-            return HttpResponse("account create done")
+            return HttpResponse("account create done!!!!")
     else:
         form = UserCreationForm()
         print("user came to form")
     return render(request, 'signup.html', {'form': form})
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username = username, password= password)
+            django_login(request, user)
+            return HttpResponse("account login done!!!!")
+    else:
+        form = AuthenticationForm()
+        print("user came to form")
+    return render(request, 'login.html', {'form': form})
